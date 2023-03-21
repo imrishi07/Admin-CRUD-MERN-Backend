baab8d89324a1914124f09198e1a7ef663381551
Rishi Anand <67603086+imrishi07@users.noreply.github.com>
2023-03-21 10:56:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const router = require("./routes/router");
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("./uploads/"));
+app.use("/uploads", express.static("./uploads"));
 app.use(router);
 
 const PORT = process.env.PORT || 8000;
